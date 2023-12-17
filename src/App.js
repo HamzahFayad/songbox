7c0b4704c3f0d228adbe9cb52bbe69ab9a0de6f6
Hamzah Fayad <hamzah.fayad@glow-berlin.de>
2023-12-17 11:22:15 +0100

    Header start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import "./App.css";
+import "./App.min.css";
+import Navbar from "./Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="App grid-width-wide">
+      <Navbar />
+      <section className="Header space-top grid-width-wide">
+        <div className="Header-left">
+          <h1>
+            Jukebox and <br /> Rhythms
+          </h1>
+          <p>
+            These headlines and slogans aim to capture the essence of blending
+            retro aesthetics with modern functionality in your web app.
+          </p>
+        </div>
+        <div className="Header-right">
+          <div>
+            <p>Hello</p>
+          </div>
+          <div>
+            <p>Hello</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+  return (
+    <nav className="nav">
+      <ul className="nav-list">
+        <li className="h3-style app-name">
+          <a href="/">
+            <span className="top">&#8514;</span>
+            SONG BOX
+            <span className="btm">&#8514;</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
